471de070937715414dc89c369a93b3de390ad25d
Pehuen <pehuen-testa@hotmail.com>
2020-04-07 22:06:33 -0300

    Endless platforms

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -8,58 +8,103 @@ public class PlatformManager : MonoBehaviour
     [SerializeField] int colums;
     [SerializeField] float horizontalSpacing;
     [SerializeField] float verticalSpacing;
+    [SerializeField] float maxRowDistance;
     [SerializeField] Vector3 spawnCenter;
 
-    [SerializeField] Transform spawnDistancePosition;
+    Transform cameraTransform;
 
-    List<List<PoolObject>> poolObjects = new List<List<PoolObject>>();
+    int lastRowAdded = 0;
+    int firstRowAdded = 0;
+
     Pool platformsPool;
+
+    Queue<List<PoolObject>> platformObjects = new Queue<List<PoolObject>>();
 
     private void Start()
     {
         platformsPool = PoolManager.GetInstance().GetPool("PlatformPool");
 
+        cameraTransform = Camera.main.transform;
+
+
+        maxRowDistance = (rows / 2) * verticalSpacing;
+
+
+
         for (int i = 0; i < rows; i++)
         {
-            int randomPlatforms = Random.Range(1, colums);
-            
-
-            int[] randomSelection = new int[colums];
-            
-
-            do
-            {
-                int selector = Random.Range(0, randomSelection.Length);
-
-                if (randomSelection[selector] == 0)
-                {
-                    randomSelection[selector] = 1;
-                    randomPlatforms--;
-                }
-
-            } while (randomPlatforms > 0);
-            
-
-            for (int j = 0; j < randomSelection.Length; j++)
-            {
-                if (randomSelection[j] == 1)
-                {
-                    PoolObject poolObject = platformsPool.GetPooledObject();
-                    poolObject.transform.position = new Vector3(spawnCenter.x + (horizontalSpacing * j), 
-                                                                spawnCenter.y + (verticalSpacing * i), 
-                                                                spawnCenter.z);
-                }
-            }
-
+            AddRow();
         }
+
     }
 
     void Update()
     {
-        Vector3 targetPosition = new Vector3(spawnDistancePosition.position.x, spawnDistancePosition.position.y, spawnCenter.z);
+        Vector3 firstRowPos = new Vector3(spawnCenter.x,
+                                          spawnCenter.y + (firstRowAdded * verticalSpacing),
+                                          spawnCenter.z);
 
-        /*new Vector3(spawnCenter.x + (horizontalSpacing * j), 
-                                                                spawnDistancePosition.position.y - (((float)rows / 2f) * verticalSpacing) + (verticalSpacing * i), 
-                                                                spawnCenter.z);*/
+        // Distance between the camera and the first platform
+        float dist = cameraTransform.position.y - firstRowPos.y;
+
+        Debug.Log(dist);
+
+        if (cameraTransform.position.y > firstRowPos.y && maxRowDistance < dist)
+        {
+            Debug.Log("hola");
+            RemoveFirstRow();
+            AddRow();
+        }
+    }
+
+    void AddRow()
+    {
+        int randomPlatforms = Random.Range(1, colums);
+
+
+        int[] randomSelection = new int[colums];
+
+
+        do
+        {
+            int selector = Random.Range(0, randomSelection.Length);
+
+            if (randomSelection[selector] == 0)
+            {
+                randomSelection[selector] = 1;
+                randomPlatforms--;
+            }
+
+        } while (randomPlatforms > 0);
+
+
+        List<PoolObject> platformsRow = new List<PoolObject>();
+
+        for (int j = 0; j < randomSelection.Length; j++)
+        {
+            if (randomSelection[j] == 1)
+            {
+                PoolObject poolObject = platformsPool.GetPooledObject();
+                poolObject.transform.position = new Vector3(spawnCenter.x + (horizontalSpacing * j),
+                                                            spawnCenter.y + (verticalSpacing * lastRowAdded),
+                                                            spawnCenter.z);
+
+                platformsRow.Add(poolObject);
+            }
+        }
+
+        lastRowAdded++;
+
+        platformObjects.Enqueue(platformsRow);
+    }
+
+    void RemoveFirstRow()
+    {
+        List<PoolObject> firstRow = platformObjects.Dequeue();
+
+        for (int i = 0; i < firstRow.Count; i++)
+            firstRow[i].Recycle();
+
+        firstRowAdded++;
     }
 }
